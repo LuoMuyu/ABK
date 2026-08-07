f41b23d3c5bc366e8e66414151e249fec7b5af1c
xingguangcuican6666 <xingguangcuican666@foxmail.com>
2026-08-07 18:13:36 +0800

    Fix isolated process startup guard

--- app/src/main/java/com/abk/kernel/AbkApplication.kt
+++ app/src/main/java/com/abk/kernel/AbkApplication.kt
@@ -4,11 +4,17 @@ import android.app.Application
 import android.content.Context
 import android.os.Process
 import android.os.SystemClock
-import android.os.UserHandle
 import com.abk.kernel.utils.LocaleHelper
 import com.abk.kernel.utils.NotificationUtils
 import com.abk.kernel.utils.RootUtils
 import com.abk.kernel.utils.WorkflowStepI18n
+
+// Mirrors AOSP isolated UID ranges without calling newer Process APIs.
+private const val PER_USER_RANGE = 100000
+private const val FIRST_APP_ZYGOTE_ISOLATED_UID = 90000
+private const val LAST_APP_ZYGOTE_ISOLATED_UID = 98999
+private const val FIRST_ISOLATED_UID = 99000
+private const val LAST_ISOLATED_UID = 99999
 
 class AbkApplication : Application() {
     companion object {
@@ -18,12 +24,13 @@ class AbkApplication : Application() {
     }
 
     override fun attachBaseContext(base: Context) {
-        super.attachBaseContext(LocaleHelper.applyLocale(base))
+        val attachedBase = if (isIsolatedProcess()) base else LocaleHelper.applyLocale(base)
+        super.attachBaseContext(attachedBase)
     }
 
     override fun onCreate() {
         super.onCreate()
-        if (UserHandle.isIsolated(Process.myUid())) {
+        if (isIsolatedProcess()) {
             return
         }
         LocaleHelper.init(this)
@@ -31,4 +38,10 @@ class AbkApplication : Application() {
         RootUtils.init(this)
         NotificationUtils.createChannels(this)
     }
+}
+
+private fun isIsolatedProcess(): Boolean {
+    val appId = Process.myUid() % PER_USER_RANGE
+    return appId in FIRST_APP_ZYGOTE_ISOLATED_UID..LAST_APP_ZYGOTE_ISOLATED_UID ||
+        appId in FIRST_ISOLATED_UID..LAST_ISOLATED_UID
 }
